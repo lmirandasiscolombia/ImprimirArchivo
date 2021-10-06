5c252ab36a584bf09d6e93ff1c3f03b31c40a44a
lmirandasiscolombia <90800486+lmirandasiscolombia@users.noreply.github.com>
2021-10-06 15:45:28 -0500

    Seleccionar cualquier archivo e imprimir

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing.Printing;
 using System.Windows.Forms;
+ 
 
 namespace ImprimirArchivo
 {
@@ -15,6 +11,41 @@ namespace ImprimirArchivo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+         
+
+           /* PrintDialog pd = new PrintDialog();
+            pd.PrinterSettings = new PrinterSettings();
+            pd.PrinterSettings.PrinterName = "doPDF11";
+           */
+            Process printjob = new Process();
+            printjob.StartInfo.FileName = openFileDialog1.FileName;
+            //printjob.StartInfo.Arguments = pd.PrinterSettings.PrinterName;
+            printjob.StartInfo.UseShellExecute = true;
+            printjob.StartInfo.CreateNoWindow = true;
+            printjob.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            printjob.StartInfo.Verb = "Print";
+            printjob.Start();
+
+        }
+
+     
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
         }
     }
 }
